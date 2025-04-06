4cb3d15ea9de3ace8eaccc82c576d66fa9786866
Jose Andres <andres.henao9@udea.edu.co>
2025-04-06 14:28:23 -0500

    Práctica de Widgets en la calculadora.

--- lib/pages/calculadora_page.dart
+++ lib/pages/calculadora_page.dart
@@ -8,8 +8,52 @@ class CalculadoraPage extends StatefulWidget {
 }
 
 class _CalculadoraPageState extends State<CalculadoraPage> {
+  int _total = 0;
+  final _numero1 = TextEditingController();
+  final _numero2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Calculadora'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _numero1,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Digite el primer número: ",
+                ),
+                keyboardType: TextInputType.number,
+              ),
+
+              const SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                controller: _numero2,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Digite el segundo número: ",
+                ),
+                keyboardType: TextInputType.number,
+              ),
+
+
+              Text(
+                "El total de la suma es: $_total ",
+                style: const TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
